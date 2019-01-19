b0278b334a6524708f1804387908b47baa69e5e7
Sean Lucius <seanlucius22@icloud.com>
2019-01-19 01:37:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,6 +2,10 @@ def my_select(collection)
  selection_array = []
  i = 0 
  while i < collection.length
-  
+  if yield(collection[i]) 
+    selection_array.push(yield(collection[i]))
+  end 
+  i += 1 
  end 
+ selection_array
 end
